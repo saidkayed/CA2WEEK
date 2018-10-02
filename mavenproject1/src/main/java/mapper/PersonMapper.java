e21935b92e86d083dac63b35a00f0861d35f401b
tobbe <tobbejj@hotmail.com>
2018-10-02 11:59:52 +0200

    facade

--- mavenproject1/src/main/java/mapper/PersonMapper.java
+++ mavenproject1/src/main/java/mapper/PersonMapper.java
@@ -1,7 +1,9 @@
 package mapper;
 
 import dto.PersonDTO;
+import entity.CityInfo;
 import entity.Person;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
@@ -34,6 +36,57 @@ public class PersonMapper {
             query.setParameter("phone", number);
             return (PersonDTO) query.getSingleResult();
         } finally {
+            em.close();
+        }
+    }
+    
+    public List<PersonDTO> findPersonsWithGivenHobby(String hobbyname){
+        EntityManager em = getEntityManager();
+        try{
+            TypedQuery<PersonDTO> query = em.createQuery("select new dto.PersonDTO(c1.id, c1.email, c1.firstName, c1.lastName)"
+                    + " from Person c1 inner join c1.hobbys as c2"
+                    + " where c2.name = :hobby", PersonDTO.class);
+            query.setParameter("hobby", hobbyname);
+            List<PersonDTO> list = query.getResultList();
+            return list;
+        }finally{
+            em.close();
+        }
+    }
+    
+    public List<PersonDTO> findPersonsWithGivenCity(String cityname){
+        EntityManager em = getEntityManager();
+        try{
+            TypedQuery<PersonDTO> query = em.createQuery("select new dto.PersonDTO(c1.id, c1.email, c1.firstName, c1.lastName)"
+                    + " from Person c1 inner join c1.address as c2"
+                    + " inner join c2.cityinfo as c3"
+                    + " where c3.city = :city", PersonDTO.class);
+            query.setParameter("city", cityname);
+            List<PersonDTO> list = query.getResultList();
+            return list;
+        }finally{
+            em.close();
+        }
+    }
+    
+    public Integer getCountOfPeopleWithGivenHobby(String hobbyname){
+        EntityManager em = getEntityManager();
+        try{
+            Query query = em.createQuery("Select count(c) from Person c where c.hobbys.name = :hobby");
+            query.setParameter("hobby", hobbyname);
+            return (Integer) query.getSingleResult();
+        }finally{
+            em.close();
+        }
+    }
+    
+    public List<Integer> getAllZipCodes(){
+        EntityManager em = getEntityManager();
+        try{
+            Query query = em.createQuery("Select c.zipCode from CityInfo c");
+            List<Integer> list = query.getResultList();
+            return list;
+        }finally{
             em.close();
         }
     }
